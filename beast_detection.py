fefff02fab50187f2cf992eedfd0223b28b2aa32
Jimmy Nguyen <jimmyn6565@gmail.com>
2024-11-16 01:06:41 -0800

    prevents objects from getting placed close to each other

--- beast_detection.py
+++ beast_detection.py
@@ -4,14 +4,17 @@ import cv2
 import numpy as np 
 import pprint
 import os
+import re
 import tempfile
 import math
 import random
 import time
 import unreal
+import pyautogui
 
 IMG_WIDTH, IMG_HEIGHT = 640, 640
 
+# used for positioning CAMERA not objects
 def get_random_coords(y_range, x_range, z_range):
     random_y = random.randint(*y_range)
     random_x = random.randint(*x_range)
@@ -24,64 +27,45 @@ def get_random_rotation():
     yaw_radians = yaw_degrees * (3.14159 / 180)
     return airsim.Quaternionr(0, 0, yaw_radians)
 
-def get_random_position(first_corner, second_corner, z=0):
+def get_random_position(first_corner, second_corner, z=1):
     # Generate random x and y within the bounds of the box
     x = random.uniform(first_corner[0], second_corner[0])
     y = random.uniform(first_corner[1], second_corner[1])
     return airsim.Vector3r(x, y, z)
 
-def boxes_overlap(box1, box2):
-    # Check if two Box2D objects overlap
-    # true if boxes overlap, false if do not
-    return not (box1.max.x_val < box2.min.x_val or
-                box1.min.x_val > box2.max.x_val or
-                box1.max.y_val < box2.min.y_val or
-                box1.min.y_val > box2.max.y_val)
+def distance_between_positions(pos1, pos2):
+    # calc euclidean distance between two positions
+    return math.sqrt((pos1.x_val - pos2.x_val)**2 + (pos1.y_val - pos2.y_val)**2)
 
-def place_objects_randomly(object_pool, first_corner, second_corner, max_num, z=0):
+def get_valid_position(first_corner, second_corner, existing_locations, min_distance):
+    valid_position = False
+    while not valid_position:
+        # generate a random position
+        new_position = get_random_position(first_corner, second_corner)
+
+        # check if it is far enough from all existing positions
+        valid_position = all(
+            distance_between_positions(new_position, loc) >= min_distance
+            for loc in existing_locations
+        )
+        if valid_position:
+            return new_position
+
+def place_objects_randomly(object_pool, first_corner, second_corner, max_num):
     # randomly select max_num objects from pool
     selected_objects = random.sample(object_pool, min(max_num, len(object_pool)))
+    # empty list of poses (object locations)
+    random_locations =  []
 
     for obj in selected_objects:
-        # small bug where SK_cars at z=0 float a little above the ground
-        position = get_random_position(first_corner, second_corner, z=0)
+        # get valid position
+        position = get_valid_position(first_corner, second_corner, random_locations, min_distance=6)    # tweak min_distance to separate objects
         orientation = get_random_rotation()
-        object_pose = airsim.Pose(position, orientation)
+        # add to random_locations list
+        random_locations.append(position)
+        # before adding pose to list of locations, check to make sure they are far enough apart from each other
+        object_pose = airsim.Pose(position, orientation)             
         client.simSetObjectPose(obj, object_pose, True)
-        # print(f"Placed {obj} at {position} with orientation {orientation}")
-    
-    # after placing objects
-    detections = client.simGetDetections(camera_name, image_type)
-
-# def place_objects_randomly(object_pool, first_corner, second_corner, max_num, z=0):
-#     selected_objects = random.sample(object_pool, min(max_num, len(object_pool)))
-#     placed_boxes = []
-
-#     for obj in selected_objects:
-#         overlap_found = True
-#         while overlap_found:
-#             position = get_random_position(first_corner, second_corner, z=0)
-#             orientation = get_random_rotation()
-#             object_pose = airsim.Pose(position, orientation)
-#             client.simSetObjectPose(obj, object_pose, True)
-
-#             # Get the new Box2D after placing the object
-#             last_object = client.simGetDetections(camera_name, image_type)[-1]
-#             current_box = last_object.box2D
-            
-#             overlap_found = False  # Assume no overlap initially
-
-#             for box in placed_boxes:
-#                 if boxes_overlap(current_box, box):  # If an overlap is found
-#                     overlap_found = True
-#                     break  # Exit the loop if an overlap is detected
-
-#             if not overlap_found:
-#                 placed_boxes.append(current_box) 
-#     # Output final positions and their boxes
-#     for box in placed_boxes:
-#         print(f"Placed object with box: {box}")
-
 
 def move_cam(cam_quarter, camera_name):
     # move camera randomly
@@ -93,6 +77,7 @@ def move_cam(cam_quarter, camera_name):
     client.simSetCameraPose(camera_name, camera_pose)
 
 def save_box_info(objects, counter):
+    class_id = "#"
     if objects:
         bbox_path = os.path.join(temp_dir, f"detection_{counter}.txt")
         with open(bbox_path, 'w') as f:
@@ -100,14 +85,19 @@ def save_box_info(objects, counter):
                 x_min, y_min = int(obj.box2D.min.x_val), int(obj.box2D.min.y_val)
                 x_max, y_max = int(obj.box2D.max.x_val), int(obj.box2D.max.y_val)
                 # x_min, y_min, x_max, y_max
-                f.write(f"{obj.name} {x_min} {y_min} {x_max} {y_max}\n")
+                # f.write(f"{obj.name} {x_min} {y_min} {x_max} {y_max}\n")
 
                 # yolo format: x_center, y_center, width, height
                 x_center = (x_min + x_max / 2) / IMG_WIDTH
                 y_center = (y_min + y_max / 2) / IMG_HEIGHT
                 width = x_max - x_min / IMG_WIDTH   # divide by width to normalize
                 height = y_max - y_min / IMG_HEIGHT
-                f.write(f"{obj.name} {x_center} {y_center} {width} {height}\n")
+
+                # if checks for obj.name
+                if re.match(r'SK_.*', obj.name):
+                    class_id = "0"
+
+                f.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
 
                 # Draw the bounding box on the image
                 # cv2.rectangle(png, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)  # Blue color with thickness of 2
@@ -125,7 +115,7 @@ def reset_objects_to_origin(object_pool):
         client.simSetObjectPose(obj, object_pose)
 
 # connect to the AirSim simulator
-client = airsim.VehicleClient()
+client = airsim.client.VehicleClient()
 client.confirmConnection()
 
 # set camera name and image type to request images and detections
@@ -162,7 +152,7 @@ client.simSetCameraPose(camera_name, camera_pose)
 # set detection radius in [cm]
 client.simSetDetectionFilterRadius(camera_name, image_type, 200 * 50) 
 # add desired object name to detect in wild card/regex format
-client.simAddDetectionFilterMeshName(camera_name, image_type, "SM_*") 
+client.simAddDetectionFilterMeshName(camera_name, image_type, "SK_*") 
 
 # create a small openCV window
 cv2.namedWindow("AirSim", cv2.WINDOW_NORMAL)
@@ -172,6 +162,7 @@ temp_dir = os.path.join(tempfile.gettempdir(), "dataset_images")
 print("Images will be saved to %s" % temp_dir)
 
 image_counter = 0
+DATASET_LENGTH = 2000
 try:
     os.makedirs(temp_dir)
 except OSError:
@@ -179,32 +170,32 @@ except OSError:
         raise
 
 # Instantiate object pool and define placement box
-object_pool = client.simListSceneObjects("SM_.*")
+object_pool = client.simListSceneObjects("SK_.*")   # change this to SM_* if on beast
 print(object_pool)
 MAX_OBJ_NUM = 5
 first_corner = (-40, -80)
 second_corner = (-60, -105)  # change this
 
-while True:
+while True and image_counter <= DATASET_LENGTH:
     rawImage = client.simGetImage(camera_name, image_type)
     if not rawImage:
         continue
     png = cv2.imdecode(airsim.string_to_uint8_array(rawImage), cv2.IMREAD_UNCHANGED)
     objects = client.simGetDetections(camera_name, image_type)
-    # if objects:
-    #     # save bounding box info to .txt file for all objects in frame
-    #     save_box_info(objects, image_counter)
-    #     # snap pic
-    #     save_frame(png, image_counter)
-    #     image_counter += 1
+    if objects:
+        # save bounding box info to .txt file for all objects in frame
+        save_box_info(objects, image_counter)
+        # snap pic
+        save_frame(png, image_counter)
+        image_counter += 1
 
     cv2.imshow("AirSim", png)
 
-    # move_cam(cam_quarter, camera_name)
+    move_cam(cam_quarter, camera_name)
 
     place_objects_randomly(object_pool, first_corner, second_corner, MAX_OBJ_NUM)
 
-    # print(client.simGetDetections(camera_name, image_type))
+    pyautogui.press('space')
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -212,7 +203,7 @@ while True:
     elif cv2.waitKey(1) & 0xFF == ord('c'):
         client.simClearDetectionMeshNames(camera_name, image_type)
     elif cv2.waitKey(1) & 0xFF == ord('a'):
-        client.simAddDetectionFilterMeshName(camera_name, image_type, "SM_*")
+        client.simAddDetectionFilterMeshName(camera_name, image_type, "SK_*")
     elif cv2.waitKey(1) & 0xFF == ord('p'):
         # save image to the temp folder
         image_path = os.path.join(temp_dir, f"frame_{image_counter}.png")
@@ -224,8 +215,9 @@ while True:
         current_pos = client.simGetCameraInfo(camera_name)
         print(current_pos)
 
-    time.sleep(1)
+    time.sleep(0.1)
     # reset_objects_to_origin(object_pool)
     # resetting objects to origin results in bug where objects do not appear on cv window output
 
 cv2.destroyAllWindows() 
+
