f4c612e75281c9a6c2745158aee0de50e54ab825
Jimmy Nguyen <jimmyn6565@gmail.com>
2024-11-23 04:52:52 -0800

    Update beast_detection.py

--- beast_detection.py
+++ beast_detection.py
@@ -13,10 +13,40 @@ import unreal
 import pyautogui
 import functions
 
+# VARIABLES
+###############################################
+###############################################
 IMG = {
     "width": 640,
     "height": 640
 }
+
+# OBJECT STRINGS (regex from name of object within UE environment)
+###############################################
+car = "SK_.*"
+soccer_ball = "football.*"
+football = "Wilson_.*"
+mattress = "Matress.*"
+baseball_bat = "Baseball.*"
+suitcase = "luggage.*"
+umbrella = "Black_Umbrella.*"
+volleyball = "volleyball.*"
+tennis_r = "tenis_.*"
+stop_sign = "Stop_.*"
+motorcycle = "motorcycle.*"
+mannequin = "Default_.*"
+boat = "small_boat_.*"
+basketball = "Basketball_.*"
+###############################################
+# Add variables above to obj_list
+obj_list = [car, soccer_ball, football, mattress, baseball_bat, suitcase, umbrella, volleyball,
+            tennis_r, stop_sign, motorcycle, mannequin, boat, basketball]
+mesh_list = [obj.replace(".", "") for obj in obj_list]
+# variable used to keep track of items left for each object
+num_objs = {obj: 1500 for obj in obj_list}
+print(num_objs)
+###############################################
+###############################################
 
 # connect to the AirSim simulator
 client = airsim.client.VehicleClient()
@@ -36,7 +66,7 @@ car_vectors = [
 ]
 x_range = (-68, -37)   # (-65, -40)     solid lines, can tweak a little to get them closer to the edge
 y_range = (-108, -77)    # (-105, -80)
-z_range = (-50, -40)    # (-50, -45) 
+z_range = (-40, -40)    # (-50, -45) 
 ranges = [x_range, y_range, z_range]
 
 car_quarterns = [
@@ -51,13 +81,12 @@ camera_pose = airsim.Pose(
     airsim.Vector3r(-50, -95, -40),
     cam_quarter
 )
-# set camera above objects
-client.simSetCameraPose(camera_name, camera_pose)
 
+client.simClearDetectionMeshNames(camera_name, image_type)
 # set detection radius in [cm]
-client.simSetDetectionFilterRadius(camera_name, image_type, 200 * 50) 
+client.simSetDetectionFilterRadius(camera_name, image_type, 80 * 100) 
 # add desired object name to detect in wild card/regex format
-client.simAddDetectionFilterMeshName(camera_name, image_type, "SK_*") 
+functions.add_mesh_filters(client, camera_name, image_type, mesh_list)
 
 # create a small openCV window
 cv2.namedWindow("AirSim", cv2.WINDOW_NORMAL)
@@ -67,7 +96,7 @@ temp_dir = os.path.join(tempfile.gettempdir(), "dataset_images")
 print("Images will be saved to %s" % temp_dir)
 
 image_counter = 0
-DATASET_LENGTH = 2000
+DATASET_LENGTH = 500
 try:
     os.makedirs(temp_dir)
 except OSError:
@@ -75,11 +104,17 @@ except OSError:
         raise
 
 # Instantiate object pool and define placement box
-object_pool = client.simListSceneObjects("SK_.*")   # change this to SM_* if on beast
+# object_pool = client.simListSceneObjects("SK_.*")   # change this to SM_* if on beast
+object_pool = functions.generate_object_pool(client, obj_list)
+print("OBJECT POOL: ")
 print(object_pool)
-MAX_OBJ_NUM = 5
-first_corner = (-35, -75)
-second_corner = (-70, -110)  # change this
+MAX_OBJ_NUM = 7
+first_corner = (-35, -75) # <-- for all objects (closer cam)        # (-35, -75) , for normal cars
+second_corner = (-70, -110) # <-- for all objects (closer cam)      # (-70, -110)
+
+
+# set starting camera position
+client.simSetCameraPose(camera_name, camera_pose)
 
 while True and image_counter < DATASET_LENGTH:
     rawImage = client.simGetImage(camera_name, image_type)
@@ -89,7 +124,7 @@ while True and image_counter < DATASET_LENGTH:
     objects = client.simGetDetections(camera_name, image_type)
     if objects:
         # save bounding box info to .txt file for all objects in frame
-        functions.save_box_info(objects, image_counter, IMG, temp_dir)
+        functions.save_box_info(objects, image_counter, png, IMG, temp_dir)
         functions.save_frame(png, image_counter, temp_dir)
         image_counter += 1
 
@@ -99,7 +134,8 @@ while True and image_counter < DATASET_LENGTH:
 
     functions.move_cam(cam_quarter, camera_name, ranges, client)
 
-    functions.place_objects_randomly(object_pool, first_corner, second_corner, MAX_OBJ_NUM, client)
+    # functions.place_objects_randomly(object_pool, first_corner, second_corner, MAX_OBJ_NUM, client)
+    functions.place_objects_randomly(object_pool, num_objs, first_corner, second_corner, MAX_OBJ_NUM, client)
 
     pyautogui.press('space')
 
@@ -121,7 +157,7 @@ while True and image_counter < DATASET_LENGTH:
         current_pos = client.simGetCameraInfo(camera_name)
         print(current_pos)
 
-    time.sleep(0.1)
+    time.sleep(.1)
     # resetting objects to origin results in bug where objects do not appear on cv window output
 
 cv2.destroyAllWindows() 
